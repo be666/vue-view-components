0f00081ae23763488919bce2be64e972a15e4bd3
bqxu <bcaring@163.com>
2016-07-08 23:55:02 +0800

    Update 0.0.6

--- src/components/checkbox.vue
+++ src/components/checkbox.vue
@@ -1,13 +1,13 @@
 <template xmlns:v-bind="http://www.w3.org/1999/xhtml" xmlns:v-on="http://www.w3.org/1999/xhtml">
   <div class="i-checkbox" v-for="item in itemList" v-bind:class="className">
-    <input type="checkbox" v-model="choose" value="{{item[sid]}}" v-on:change="_change">
+    <input type="checkbox" v-model="selected" value="{{item[id]}}" v-on:change="_change">
     {{item[text]}}
   </div>
 </template>
 <script>
   export default {
     name: 'i_checkbox',
-    props: ['itemList', 'choose', 'sid', 'text', 'className'],
+    props: ['itemList', 'selected', 'id', 'text', 'className'],
     methods: {
       _change(){
         this.$dispatch('change');
--- src/components/radio.vue
+++ src/components/radio.vue
@@ -1,12 +1,12 @@
-<template>
-  <div class="i-radio" v-for="item in itemList">
-    <input type="radio" v-model="choose" value="{{item[sid]}}">
+<template xmlns:v-bind="http://www.w3.org/1999/xhtml">
+  <div class="i-radio" v-for="item in itemList" v-bind:class="className">
+    <input type="radio" v-model="selected" value="{{item[id]}}">
     {{item[text]}}
   </div>
 </template>
 <script>
   export default {
     name: 'i_radio',
-    props: ['itemList', 'choose', 'sid', 'text']
+    props: ['itemList', 'selected', 'id', 'text', 'className']
   }
 </script>
